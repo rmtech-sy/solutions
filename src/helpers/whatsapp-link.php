5f44e9e5ff368962d376ab035f1689fd59ba882f
RMTech <rogerio@rmtechsolutions.com.br>
2025-06-12 21:41:40 -0300

    Delete src/helpers directory

--- src/helpers/whatsapp-link.php
+++
@@ -1,6 +0,0 @@
-<?php
-function generateWhatsAppLink($phoneNumber, $message = '') {
-    $baseUrl = 'https://wa.me/';
-    $encodedMessage = urlencode($message);
-    return $baseUrl . $phoneNumber . '?text=' . $encodedMessage;
-}
